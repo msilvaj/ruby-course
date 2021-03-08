e3e610f621ec1c9a0e821fc404f3e79261aaa953
msilvaj <miqueias.ufpi@gmail.com>
2021-03-08 15:45:33 -0300

    Final steps followed

--- montys-color.rb
+++ montys-color.rb
@@ -1,7 +1,7 @@
 # Lesson 3: Prompt for Favorite Color
 
  puts "What is your favorite color?"
- color = gets.chop
+ color = gets.chop.downcase
 # puts "Wow! My favorite color is #{color} also!"
 
 # Lesson 4: Using an If Statement
@@ -12,8 +12,10 @@
 
 # Lesson 5: Adding an Else Statement
 
- if color == "blue"
+ if color == "blue" || color == "green"
      puts "Wow! My favorite color is #{color} also!"
+ elsif collor == "red"
+   puts "It's not as good as blue, but red isn't too bad."
  else
      puts "I'm not a big fan of #{color}."
  end
